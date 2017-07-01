f4a7274088a563825bae61c18f878c05c89644a9
Flavia Ferreira <flaviahferreirah@gmail.com>
2017-07-01 14:56:09 -0300

    Ajustando projeto

--- jsfPosJava/src/main/java/controle/EstadoControle.java
+++ jsfPosJava/src/main/java/controle/EstadoControle.java
@@ -1,21 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controle;
 
-import entidade.Estado;
+import enetidade.Estado;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+/**
+ *
+ * @author UniCesumar
+ */
 @ManagedBean
 @SessionScoped
 public class EstadoControle {
+
     private Estado estado = new Estado();
-    
-    private List<Estado> listaEstados = new ArrayList<Estado>();
-    
-    public void salvar() {
-        listaEstados.add(estado);
-        estado = new Estado();
+    private List<Estado> lista = new ArrayList<Estado>();
+
+    public List<Estado> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Estado> lista) {
+        this.lista = lista;
     }
 
     public Estado getEstado() {
@@ -26,12 +38,20 @@ public class EstadoControle {
         this.estado = estado;
     }
 
-    public List<Estado> getListaEstados() {
-        return listaEstados;
+    public void salvar() {
+        lista.add(estado);
     }
 
-    public void setListaEstados(List<Estado> listaEstados) {
-        this.listaEstados = listaEstados;
+    public void novo() {
+        estado = new Estado();
     }
-    
+
+    public void excluir(Estado e) {
+        lista.remove(e);
+    }
+
+    public void alterar(Estado e) {
+        this.estado = e;
+    }
+
 }
