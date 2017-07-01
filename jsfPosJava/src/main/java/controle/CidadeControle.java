148724d61dfd25a86438fa5e047500e718679dae
Flavia Ferreira <flaviahferreirah@gmail.com>
2017-07-01 15:53:36 -0300

    Ajustando projeto

--- jsfPosJava/src/main/java/controle/CidadeControle.java
+++ jsfPosJava/src/main/java/controle/CidadeControle.java
@@ -1,5 +1,6 @@
 package controle;
 
+import converter.EstadoConverter;
 import enetidade.Cidade;
 import enetidade.Estado;
 import java.util.ArrayList;
@@ -12,9 +13,20 @@ import javax.faces.bean.SessionScoped;
 public class CidadeControle {
     
     private Cidade cidade = new Cidade();
-    private Estado estado = new Estado();
-    
+    private EstadoConverter estadoConverter = new EstadoConverter();
     private List<Cidade> lista = new ArrayList<>();
+    
+    public EstadoConverter getEstadoConverter() {
+        if(estadoConverter == null) {
+            estadoConverter = new EstadoConverter();
+        }
+        
+        return estadoConverter;
+    }
+    
+    public void setEstadoconverter(EstadoConverter estadoConverter) {
+        this.estadoConverter = estadoConverter;
+    }
 
     public Cidade getCidade() {
         return cidade;
@@ -22,14 +34,6 @@ public class CidadeControle {
 
     public void setCidade(Cidade cidade) {
         this.cidade = cidade;
-    }
-
-    public Estado getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Estado estado) {
-        this.estado = estado;
     }
 
     public List<Cidade> getLista() {
@@ -46,7 +50,6 @@ public class CidadeControle {
 
     public void novo() {
         cidade = new Cidade();
-        estado = new Estado();
     }
 
     public void excluir(Cidade c) {
